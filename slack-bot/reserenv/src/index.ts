2b4bab640eadd715784a00e19b577da65c04394a
Cristopher <cristophernamchee12@gmail.com>
2025-10-01 21:21:25 +0700

    refactor: separate by command type

--- slack-bot/reserenv/src/index.ts
+++ slack-bot/reserenv/src/index.ts
@@ -1,8 +1,7 @@
 import { Hono } from 'hono';
-import type { Bindings } from './types';
-import { generateEnvironmentTables } from './utils';
 
-const ENVIRONMENTS = ['dev', 'dev2', 'dev3'];
+import reservation from './commands/reservation';
+import type { Bindings } from './types';
 
 const app = new Hono<{ Bindings: Bindings }>();
 
@@ -24,52 +23,6 @@ app.post('/commands/unreserve', async (c) => {
   return c.text('OK');
 });
 
-app.post('commands/reservation', async (c) => {
-  const { text } = await c.req.parseBody();
-
-  let environment = '';
-  if (typeof text === 'string') {
-    const params = text.split(/\s+/);
-    environment = params[0];
-  }
-
-  if (!environment) {
-    const blockBody = await generateEnvironmentTables(ENVIRONMENTS, c.env.KV);
-
-    await fetch(c.env.SLACK_WEBHOOK_URL, {
-      body: JSON.stringify({
-        block: blockBody,
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }),
-    });
-  }
-
-  if (!ENVIRONMENTS.includes(environment)) {
-    await fetch(c.env.SLACK_WEBHOOK_URL, {
-      body: JSON.stringify({
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: "The specified 'dev' environment doesn't exist!",
-            },
-          },
-        ],
-      }),
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    return c.text('OK');
-  }
-
-  return c.text('OK');
-});
+app.post('commands/reservation', reservation);
 
 export default app;
